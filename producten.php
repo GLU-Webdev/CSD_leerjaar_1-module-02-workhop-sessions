15b7c026481571c96e988a37f3c0fb5f4ddce489
ttijs <ttijs@glu.nl>
2024-03-27 07:17:33 +0100

    af

---
+++ producten.php
@@ -0,0 +1,68 @@
+<?php
+$array = [
+    "products" => [
+        [
+            "id" => "1",
+            "title" => "Product 1",
+            "category" => "Category 1",
+            "price" => 10.00,
+			"sale" => false,
+            "photos" => [
+                "photo1" => "image1-1.jpg",
+                "photo2" => "image1-2.jpg",
+                "photo3" => "image1-3.jpg"
+            ]
+        ],
+        [
+            "id" => "2",
+            "title" => "Product 2",
+            "category" => "Category 1",
+            "price" => 20.00,
+						"sale" => 14.99,
+            "photos" => [
+                "photo1" => "image2-1.jpg",
+                "photo2" => "image2-2.jpg",
+                "photo3" => "image2-3.jpg"
+            ]
+        ],
+	    [
+            "id" => "3",
+            "title" => "Product 3",
+            "category" => "Category 2",
+            "price" => 30.00,
+						"sale" => 18.99,
+            "photos" => [
+                "photo1" => "image3-1.jpg",
+                "photo2" => "image3-2.jpg",
+                "photo3" => "image3-3.jpg"
+            ]
+            ],
+	    [
+            "id" => "4",
+            "title" => "Product 4",
+            "category" => "Category 2",
+            "price" => 33.00,
+						"sale" => 29.99,
+            "photos" => [
+                "photo1" => "image4-1.jpg",
+                "photo2" => "image4-2.jpg",
+                "photo3" => "image4-3.jpg"
+            ]
+        ]
+
+    ],
+	"navigation" => [
+        [
+            "title" => "Home",
+            "url" => "/"
+        ],
+        [
+            "title" => "Producten",
+            "url" => "/producten"
+        ],
+        [
+            "title" => "Contact",
+            "url" => "/contact"
+        ]
+    ]
+];
--- sessions.php
+++ sessions.php
@@ -7,6 +7,12 @@ if (isset($_GET['destroy'])) {
     header('Location: ' . $_SERVER['PHP_SELF']);
     exit();
 }
+
+// print_r($_POST);
+if (isset($_POST['bestel'])) {
+    $_SESSION['cart'][$_POST['id']] = $_POST['aantal'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +22,7 @@ if (isset($_GET['destroy'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
@@ -29,13 +36,16 @@ if (isset($_GET['destroy'])) {
 
     <h1>home</h1>
     <p>
+    <pre>
         <?php
-        if (isset($_SESSION['name'])) {
-            echo '$_SESSION["name"]: ' . $_SESSION['name'];
+        if (isset($_SESSION['cart'])) {
+            echo "\r\n" . '$_SESSION["cart"]:' . "\r\n";
+            print_r($_SESSION["cart"]);
         } else {
-            echo 'no name';
+            echo 'no cart';
         }
         ?>
+        </pre>
     </p>
 
 
@@ -43,9 +53,41 @@ if (isset($_GET['destroy'])) {
 
         <input type="text" name="name">
 
+
         <input type="submit" value="submit">
 
     </form>
+
+    <hr>
+
+    <h1>Producten</h1>
+
+    <div class="products">
+        <?php
+        include 'producten.php';
+
+        foreach ($array['products'] as $product) {
+            //echo $product['id'];
+        ?>
+            <div class="product" style="">
+                <a href="detail.php?id=<?= $product['id']; ?>">
+                    <div class="title"><?= $product['title']; ?></div>
+                </a>
+                <form action="" method="post">
+                    <input type="hidden" name="id" value="<?= $product['id']; ?>">
+                    <input type="submit" name="bestel" value="bestel">
+                    <input type="number" name="aantal" value="1" min=1>
+                </form>
+            </div>
+
+
+        <?php
+        }
+        ?>
+    </div>
+
+
+
 
 
 </body>
